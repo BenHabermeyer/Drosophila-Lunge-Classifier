0cfd2729b01593bc028aeff5459e006fe30cf976
Ben Habermeyer <46455195+BenHabermeyer@users.noreply.github.com>
2019-01-11 12:15:09 -0500

    Add files via upload

--- auto_calibrate.m
+++ auto_calibrate.m
@@ -1,3 +1,10 @@
 function auto_calibrate(f_vid, f_info)
+    %Ben Habermeyer
+    %Add flytracker to path then launch calibrator
+    %function takes as input the pathname to the video and the pathname to
+    %the calibration file
+    
+    flytracker_path = 'C:\Users\Ben\Documents\FlyTracker-1.0.5';
+    addpath(genpath(flytracker_path))
     success = calibrator(f_vid, f_info)
 end
--- auto_track.m
+++ auto_track.m
@@ -1,19 +1,24 @@
-% list of all folders to be processed
-function auto_track(folders,f_vid, f_calib)
-    %folders = {'C:/Users/bmain/Desktop/machine_learning/videos/30_fps/vid2/test/6-16-16 D7 31 grouped splitp1 x Trpa1/'};
-
+function auto_track(folders, f_vid, f_calib)
+    % list of all folders to be processed
+    %Ben Habermeyer
+    %Function for calling flytracker given a video and calibration file
+    %folders represents the folders containing videos to track
+    %f_vid represents the file extension .mp4
+    %f_calib represents the calibration file
+    
+    %add flytracker directory to path
+    flytracker_path = 'C:\Users\Ben\Documents\FlyTracker-1.0.5';
+    addpath(genpath(flytracker_path))
+    
     % set options (omit any or all to use default options)
     %options.granularity  = 10000;
-    options.num_chunks   = 4;       % set either granularity or num_chunks
-    options.num_cores    = 2;
+    options.num_chunks   = 4;       %set either granularity or num_chunks
+    options.num_cores    = 2;       %2 on Ben's Computer
     options.max_minutes  = Inf;
     options.save_JAABA   = 1;
-    options.save_seg     = 0;
+    options.save_seg     = 0;       %don't need segmentation
 
     % set up tracker using inputted file and f_calib
-    %videos.dir_in = f_vid;
-    %videos.dir_out = f_vid;
-    %videos.filter = 
     % loop through all folders
     for f=1:numel(folders)
         % set parameters for specific folder
@@ -21,5 +26,5 @@ function auto_track(folders,f_vid, f_calib)
         videos.dir_out = folders{f}; % save results into video folder
         videos.filter = f_vid;     % extension of the videos to process
         % track all videos within folder
-        tracker(videos,options,f_calib);
+        tracker(videos, options, f_calib);
     end
