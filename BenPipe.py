22da8a0be66a51b35f3d6d332467e3c8c14bd165
Ben Habermeyer <46455195+BenHabermeyer@users.noreply.github.com>
2019-01-11 11:36:03 -0500

    Delete BenPipe.py

--- BenPipe.py
+++
@@ -1,8 +0,0 @@
-#from BenAutomate import VideoPipelineNew
-from time import time
-import cv2
-
-
-if __name__ == '__main__':
-	s = time()
-	print('program took this long to run: ' + str(time() - s))
